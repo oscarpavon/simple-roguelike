59a2ddf7ffee41b3cf81d6a261c7c871bc441feb
Oscar Pavon <pavon_oscar@outlook.com>
2018-12-15 02:06:20 -0300

    hide cursor

--- src/gui.rs
+++ src/gui.rs
@@ -9,15 +9,18 @@ pub fn draw(){
         player_health += 1;
     }
     
+    
     let _terminal = terminal();
     //_terminal.clear(ClearType::All);
 
     let _cursor = cursor();
-	_cursor.save_position();	
+	_cursor.hide();
+    _cursor.save_position();	
 	
 	let (width, height) = _terminal.terminal_size();	
-	_cursor.goto(width/2, 0);	//always in the right corner (width - char count)
+	_cursor.goto(width - 10, 0);	//always in the right corner (width - char count)
 	println!("{}", style(format!("Health: {}", player_health))
-				   .with(Color::Red));		
+				   .with(Color::Red));	
+
 	//_cursor.reset_position(); //back to the original position for writen the other text
 }
